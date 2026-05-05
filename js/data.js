7e78b7bd2a693081b021496b41a490e0755e4386
Hapafin <pierrot.sergey1997@gmail.com>
2026-05-05 18:27:28 +0300

    Add dosage data for various medications

---
+++ js/data.js
@@ -0,0 +1,7 @@
+export const dosageData = [
+    { name: "Адреналін",          bMin: 0, bMax: 0,    bUnit: "", iMin: 0.01, iMax: 0.5,  iUnit: "мкг/кг/хв" },
+    { name: "Норадреналін",       bMin: 0, bMax: 0,    bUnit: "", iMin: 0.05, iMax: 1.0,  iUnit: "мкг/кг/хв" },
+    { name: "Добутамін",          bMin: 0, bMax: 0,    bUnit: "", iMin: 2.0,  iMax: 20,   iUnit: "мкг/кг/хв" },
+    { name: "Пропофол",           bMin: 1.5, bMax: 2.5, bUnit: "мг/кг", iMin: 0.3, iMax: 6.0, iUnit: "мг/кг/год" },
+    // ... (додай всі інші препарати)
+];
